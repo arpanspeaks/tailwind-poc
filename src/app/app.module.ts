3b52db7cb945e5504ce0c9f432d9257a116f78ee
Arpan Sardar <Arpan.Sardar@kantar.com>
2022-10-08 02:56:53 +0530

    email-subscribe page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmailSubscribeComponent } from './email-subscribe/email-subscribe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmailSubscribeComponent
   ],
   imports: [
     BrowserModule
